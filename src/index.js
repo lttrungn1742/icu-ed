95568abbbe5ead4a88a513353029afe1dcbd61cc
trung <trung.le@netlink.vn>
2024-04-05 17:26:28 +0700

    build

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Registration from "./tool/mode1";
+import Registration from "./tool/Main";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="App">
-      <Registration />
+          <Registration />
     </div>
   </React.StrictMode>
 );
--- src/tool/BaseDate.js
+++ src/tool/BaseDate.js
@@ -1,67 +1,32 @@
 import React from "react";
-import "./style.css";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
-import moment from "moment";
-import 'moment/locale/vi';
 
 
-const Registration = () => {
-  moment.locale('vi');
-  const [total, setTotal] = useState(500);
-  const [speed, setSpeed] = useState(50);
-  const [time, setTime] = useState("00:00");
-  const [date, setDate] = useState(null);
-  const [timeEnd, setTimeEnd] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
- 
+const BaseDate = () => {
 
-    if(time && date) {
-      const [hour, minute] = time.split(":");
+    const [total, setTotal] = useState();
+    const [speed, setSpeed] = useState();
+    const [time, setTime] = useState("00:00");
+    const [date, setDate] = useState(null);
+    const [timeEnd, setTimeEnd] = useState("");
+    const [dateEnd, setDateEnd] = useState(null);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+     
+  
+        const _time = (new Date(`${dateEnd} ${timeEnd}`).getTime()  - new Date(`${date} ${time}`).getTime() ) / 3600000;
 
-      var timeData = moment(date).set({hour: hour, minute});
- 
-      timeData.add(total / speed * 60, 'minutes')
-
-    
-      console.log("timeData ", timeData.format("LLLL"));
-      setTimeEnd(timeData.format("LLLL"));
-      console.log(timeData.calendar())
-    }
-  };
-
-  return (
-    <div>
-      <section
-        className="p-5 w-100"
-        style={{  borderRadius: ".5rem .5rem 0 0" }}
-      >
-        <div className="row">
- 
-          <div className="col-12">
-            <div className="card text-black" style={{ borderRadius: "25px" }}>
-              <div className="card-body p-md-5">
+        setTotal(_time * speed);
+      };
+    return (
+        <div className="card-body p-md-5" >
                 <div className="row justify-content-center">
                   <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                    
                     <form onSubmit={handleSubmit}>
                       <div className="row mt-3">
-                        <div className="col text-left">
-                          <label htmlFor="first" className="form-label">
-                            Thể tích (mL)
-                          </label>
-                          <input
-                            type="number"
-                            className="form-control"
-                            value={total}
-                            onChange={(e) => {
-                              setTotal(e.target.value)
-                            }}
-                            min={0}
-                            required
-                          />
-                        </div>
+                      
                         <div className="col text-left">
                           <label htmlFor="last`" className="form-label">
                             Tốc độ (mL/h)
@@ -107,6 +72,33 @@ const Registration = () => {
                         
                         </div>
                       </div>
+                      <div className="row mt-3">
+                        <h5 htmlFor="first" className="form-label">
+                        Thời gian kết thúc
+                        </h5>
+                        <div className="col-4 text-left">
+                
+                          <input
+                            className="form-control"
+                            type="time"
+                            min="00:00" max="23:59"
+                            value={timeEnd}
+                            onChange={(e)=>{setTimeEnd(e.target.value)}}
+                            required
+                          />
+                        
+                        </div>
+                        <div className="col text-left">
+                          <input
+                            className="form-control"
+                            type="date"
+                            value={dateEnd}
+                            onChange={(e)=>{setDateEnd(e.target.value)}}
+                            required
+                          />
+                        
+                        </div>
+                      </div>
                    
                       <div className="row mt-3">
                         <div className="col text-left actionButtons">
@@ -131,30 +123,20 @@ const Registration = () => {
                               </tr>
                             </thead>
                             <tbody>
-                              <tr>
+                              <tr className="table-success">
                                 <th scope="row">Tổng thể tích</th>
                                 <td>{total} (mL)</td>
                               </tr>
-                              <tr>
-                                <th scope="row">Tốc độ truyền</th>
-                                <td>{speed} (mL/h)</td>
-                              </tr>
-                              <tr className="table-success">
-                                <th scope="row">Thời gian kết thúc</th>
-                                <td>{timeEnd}</td>
-                              </tr>
+                              
+                  
                             </tbody>
                    </table>
                       
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+    )
+}
 
-export default Registration;
+
+export default BaseDate;
